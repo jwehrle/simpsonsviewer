3677155661b3afd53a60606d77616655073527eb
John Wehrle <wehrlej@gmail.com>
2023-08-08 11:31:56 -0700

    Added basic widget testing.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,5 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:mocktail_image_network/mocktail_image_network.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,8 +7,12 @@ import 'package:http/http.dart';
 import 'package:http/testing.dart';
 import 'package:simpsonsviewer/controllers/app_controller.dart';
 
-import 'package:simpsonsviewer/main.dart';
 import 'package:simpsonsviewer/models/character.dart';
+import 'package:simpsonsviewer/views/adaptive_layout.dart';
+import 'package:simpsonsviewer/views/character_detail.dart';
+import 'package:simpsonsviewer/views/character_list.dart';
+
+class MockAppController extends Mock implements AppController {}
 
 /// Example character map taken from successful API result.
 const Map<String, dynamic> wellFormedCharacterMap = {
@@ -58,18 +57,37 @@ const Map<String, dynamic> weirdImageCharacterMap = {
 const Map<String, dynamic> emptyCharacterMap = {};
 
 void main() {
-
   late String body;
   late List<Character> characterList;
 
   setUp(() async {
     body = await rootBundle.loadString('assets/test_response_body.json');
     characterList = [
-      Character(name: "Apu Nahasapeemapetilan", description: "Apu Nahasapeemapetilan - Apu Nahasapeemapetilan is a recurring character in the American animated television series The Simpsons. He is an Indian immigrant proprietor who runs the Kwik-E-Mart, a popular convenience store in Springfield, and is known for his catchphrase, \"Thank you, come again\".", imageURL: ""),
-      Character(name: "Apu Nahasapeemapetilon", description: "Apu Nahasapeemapetilon - Apu Nahasapeemapetilon is a recurring character in the American animated television series The Simpsons. He is an Indian immigrant proprietor who runs the Kwik-E-Mart, a popular convenience store in Springfield, and is known for his catchphrase, \"Thank you, come again\".", imageURL: "/i/99b04638.png"),
-      Character(name: "Barney Gumble", description: "Barney Gumble - Barnard Arnold \"Barney\" Gumble is a recurring character in the American animated TV series The Simpsons. He is voiced by Dan Castellaneta and first appeared in the series premiere episode \"Simpsons Roasting on an Open Fire\". Barney is the town drunk of Springfield and one of Homer Simpson's friends.", imageURL: "/i/39ce98c0.png"),
-      Character(name: "Bart Simpson", description: "Bart Simpson - Bartholomew Jojo \"Bart\" Simpson is a fictional character in the American animated television series The Simpsons and part of the Simpson family. He is voiced by Nancy Cartwright and first appeared on television in The Tracey Ullman Show short \"Good Night\" on April 19, 1987.", imageURL: ""),
-      Character(name: "Bender (Futurama)", description: "Bender (Futurama) - Bender Bending Rodríguez is one of the main characters in the animated television series Futurama. He was conceived by the series' creators Matt Groening and David X. Cohen, and is voiced by John DiMaggio.", imageURL: "/i/cb4121fd.png"),
+      Character(
+          name: "Apu Nahasapeemapetilan",
+          description:
+              "Apu Nahasapeemapetilan - Apu Nahasapeemapetilan is a recurring character in the American animated television series The Simpsons. He is an Indian immigrant proprietor who runs the Kwik-E-Mart, a popular convenience store in Springfield, and is known for his catchphrase, \"Thank you, come again\".",
+          imageURL: ""),
+      Character(
+          name: "Apu Nahasapeemapetilon",
+          description:
+              "Apu Nahasapeemapetilon - Apu Nahasapeemapetilon is a recurring character in the American animated television series The Simpsons. He is an Indian immigrant proprietor who runs the Kwik-E-Mart, a popular convenience store in Springfield, and is known for his catchphrase, \"Thank you, come again\".",
+          imageURL: "/i/99b04638.png"),
+      Character(
+          name: "Barney Gumble",
+          description:
+              "Barney Gumble - Barnard Arnold \"Barney\" Gumble is a recurring character in the American animated TV series The Simpsons. He is voiced by Dan Castellaneta and first appeared in the series premiere episode \"Simpsons Roasting on an Open Fire\". Barney is the town drunk of Springfield and one of Homer Simpson's friends.",
+          imageURL: "/i/39ce98c0.png"),
+      Character(
+          name: "Bart Simpson",
+          description:
+              "Bart Simpson - Bartholomew Jojo \"Bart\" Simpson is a fictional character in the American animated television series The Simpsons and part of the Simpson family. He is voiced by Nancy Cartwright and first appeared on television in The Tracey Ullman Show short \"Good Night\" on April 19, 1987.",
+          imageURL: ""),
+      Character(
+          name: "Bender (Futurama)",
+          description:
+              "Bender (Futurama) - Bender Bending Rodríguez is one of the main characters in the animated television series Futurama. He was conceived by the series' creators Matt Groening and David X. Cohen, and is voiced by John DiMaggio.",
+          imageURL: "/i/cb4121fd.png"),
     ];
   });
 
@@ -114,16 +132,23 @@ void main() {
   });
 
   group("AppController tests", () {
-    MockClient client = MockClient((request) => Future.value(Response(body, 200)));
+    MockClient client =
+        MockClient((request) => Future.value(Response(body, 200)));
 
     test('showName', () {
-      AppController controller = AppController(showName: "simpsons", client: client,);
+      AppController controller = AppController(
+        showName: "simpsons",
+        client: client,
+      );
       expect(controller.showName, "simpsons");
       controller.dispose();
     });
 
     test('fetchAll', () async {
-      AppController controller = AppController(showName: "simpsons", client: client,);
+      AppController controller = AppController(
+        showName: "simpsons",
+        client: client,
+      );
       final result = await controller.fetchAll();
       expect(result, characterList);
       controller.dispose();
@@ -131,7 +156,10 @@ void main() {
 
     test('select', () {
       Character character = Character.fromMap(wellFormedCharacterMap);
-      AppController controller = AppController(showName: "simpsons", client: client,);
+      AppController controller = AppController(
+        showName: "simpsons",
+        client: client,
+      );
       controller.selectedCharacter
           .addListener(() => expect(controller.selectedCharacter, character));
       controller.select = character;
@@ -139,7 +167,10 @@ void main() {
     });
 
     test('unselect', () {
-      AppController controller = AppController(showName: "simpsons", client: client,);
+      AppController controller = AppController(
+        showName: "simpsons",
+        client: client,
+      );
       controller.selectedCharacter
           .addListener(() => expect(controller.selectedCharacter, null));
       controller.select = null;
@@ -147,20 +178,54 @@ void main() {
     });
   });
 
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('AdaptiveLayout displays Character list',
+      (WidgetTester tester) async {
+    MockClient client =
+        MockClient((request) => Future.value(Response(body, 200)));
+    final controller = AppController(showName: 'Simpsons', client: client);
+    await tester
+        .pumpWidget(MaterialApp(home: AdaptiveLayout(controller: controller)));
+    expect(find.byType(CharacterList), findsOneWidget);
+    await tester.pumpAndSettle();
+    // find an item by text and tap
+    final gumbleFinder = find.widgetWithText(ListTile, 'Barney Gumble');
+    expect(gumbleFinder, findsOneWidget);
+    await tester.tap(gumbleFinder);
+    controller.dispose();
+  });
 
-    // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
+  testWidgets('CharacterDetailPhone', (WidgetTester tester) async {
+    await mockNetworkImages(
+      () async => tester.pumpWidget(
+        MaterialApp(
+          home: CharacterDetailPhone(
+            character: characterList[2],
+          ),
+        ),
+      ),
+    );
+    expect(find.byType(CharacterImage), findsOneWidget);
+    expect(find.byType(CharacterDescription), findsOneWidget);
+  });
 
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
+  testWidgets('CharacterDetailTablet', (WidgetTester tester) async {
 
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+    final ValueNotifier<Character?> selectedCharacter = ValueNotifier(characterList[2]);
+
+    await mockNetworkImages(
+      () async => tester.pumpWidget(
+        MaterialApp(
+          home: Material(
+            child: CharacterDetailTablet(
+              selectedCharacter: selectedCharacter,
+            ),
+          ),
+        ),
+      ),
+    );
+    expect(find.byType(CharacterImage), findsOneWidget);
+    expect(find.byType(CharacterDescription), findsOneWidget);
+
+    selectedCharacter.dispose();
   });
 }
